09c8464f93e302999be1663c026119e580a9c043
LiubomyrCoding <liubomyrguzhumit@gmail.com>
2026-02-01 13:09:31 -0500

    HomeWork-Libraries

--- lesson-15.js
+++ lesson-15.js
@@ -1,3 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Modal } from 'bootstrap'
+import { format } from 'date-fns'
+const textMessage = document.querySelector('.modal-body')
+
 const parent1 = document.querySelector(".parent1");
 const input = document.querySelector("input");
 const btn = document.querySelector(".btn");
@@ -6,18 +11,31 @@ let arr = JSON.parse(localStorage.getItem("Value")) || [];
 
 let counter = Number(localStorage.getItem("Counter")) || 0;
 
+let times = JSON.parse(localStorage.getItem('Time')) || []
+
+
 function saveToLocalStorage() {
   localStorage.setItem("Value", JSON.stringify(arr));
   localStorage.setItem("Counter", JSON.stringify(counter));
+  localStorage.setItem('Time', JSON.stringify(times))
 }
 
-function createListItem(text, index) {
+function createListItem(text, index, time, timeIndex) {
+  const newDiv = document.createElement('div')
+  
   const newLi = document.createElement("li");
   newLi.textContent = text;
   newLi.dataset.index = index;
+  newDiv.textContent = time
+  newDiv.dataset.index = timeIndex
+
+  newLi.append(newDiv)
+
 
   const newButton = document.createElement("button");
   newButton.textContent = "Delete";
+  parent1.setAttribute('data-bs-toggle', 'modal')
+  parent1.setAttribute('data-bs-target', '#staticBackdrop')
   newLi.append(newButton);
 
   return newLi;
@@ -27,10 +45,13 @@ btn.addEventListener("click", function () {
   if (input.value.trim() === "") 
     return;
 
+  const result = format(new Date(), 'E MMM d , h:mm a') 
+  times.push(result)
+
   arr.push(input.value);
   saveToLocalStorage();
 
-  const newLi = createListItem(input.value, arr.length -1);
+  const newLi = createListItem(input.value, arr.length - 1, result, times.length - 1);
   parent1.appendChild(newLi);
 
   counter++;
@@ -40,7 +61,7 @@ btn.addEventListener("click", function () {
 });
 
 for (let i = 0; i < arr.length; i++) {
-  const storedLi = createListItem(arr[i], i);
+  const storedLi = createListItem(arr[i], i, times[i], i);
   parent1.appendChild(storedLi);
 }
 
@@ -60,6 +81,8 @@ parent1.addEventListener("click", function (event) {
 
     arr.splice(index, 1);
 
+    times.splice(index, 1)
+
     saveToLocalStorage();
 
     li.remove();
@@ -69,4 +92,5 @@ parent1.addEventListener("click", function (event) {
       item.dataset.index = i;
     });
   }
+  textMessage.textContent = event.target.textContent
 });
